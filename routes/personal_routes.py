b58d2c0fecbd62732a561399e13ee82ef32dc946
pedroh-zaneze <153083328+pedroh-zaneze@users.noreply.github.com>
2024-10-13 14:59:11 -0300

    ajeitando as rotas do personal

--- routes/personal_routes.py
+++ routes/personal_routes.py
@@ -1,77 +1,63 @@
-from fastapi import APIRouter, Request, status
+from fastapi import APIRouter, HTTPException, Request, status
+from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-
-'''def common_view_params(request: Request, show_header=True, show_footer=True, show_sidebar=True):
-    return {
-        "request": request,
-        "show_header": show_header,
-        "show_footer": show_footer,
-        "show_sidebar": show_sidebar
-    }'''
-#
-# PERSONAL
-#
 
 router = APIRouter(prefix="/personal")
 
 templates = Jinja2Templates(directory="templates")
 
-@router.get("/personal_inicial")  
-def get_root(request: Request):
+@router.get("/")
+async def get_root(request: Request):
+    return RedirectResponse("pages/personal/pagina_inicial_personal.html", status.HTTP_303_SEE_OTHER)
 
-    return templates.TemplateResponse("pages/personal/index.html", {"request": request})
+@router.get("/inicial")  
+async def get_inicial(request: Request):
+    return templates.TemplateResponse("pages/personal/pagina_inicial_personal.html", {"request": request})
 
-@router.get("/configuracoes_personal")  
-def get_root(request: Request):
-
+@router.get("/configuracoes")  
+async def get_configuracoes(request: Request):
     return templates.TemplateResponse("pages/personal/configuracoes_personal.html", {"request": request})
 
-@router.get("/sobre_personal")  
-def get_root(request: Request):
-
+@router.get("/sobre")  
+async def get_sobre(request: Request):
     return templates.TemplateResponse("pages/personal/sobre_personal.html", {"request": request})
 
-@router.get("/termos_personal")  
-def get_root(request: Request):
-
+@router.get("/termos")  
+async def get_termos(request: Request):
     return templates.TemplateResponse("pages/personal/termos_personal.html", {"request": request})
 
-@router.get("/contato_personal")  
-def get_root(request: Request):
-
+@router.get("/contato")  
+async def contato(request: Request):
     return templates.TemplateResponse("pages/personal/contato_personal.html", {"request": request})
 
-@router.get("/suporte_personal")  
-def get_root(request: Request):
-
+@router.get("/suporte")  
+async def get_suporte(request: Request):
     return templates.TemplateResponse("pages/personal/suporte_personal.html", {"request": request})
 
-@router.get("/politicadeprivacidade_personal")  
-def get_root(request: Request):
-
+@router.get("/politicadeprivacidade")  
+async def get_politicadeprivacidade(request: Request):
     return templates.TemplateResponse("pages/personal/politicadeprivacidade_personal.html", {"request": request})
 
-@router.get("/planos_personal")  
-def get_root(request: Request):
-
+@router.get("/planos")  
+async def get_planos(request: Request):
     return templates.TemplateResponse("pages/personal/plano_personal.html", {"request": request})
 
-@router.get("/artigos_personal")  
-def get_root(request: Request):
-
+@router.get("/artigos")  
+async def get_artigos(request: Request):
     return templates.TemplateResponse("pages/personal/artigos_personal.html", {"request": request})
 
-@router.get("/adicionar_exercicio_personal")  
-def get_root(request: Request):
-
+@router.get("/adicionar_exercicio")  
+async def get_adicionar_exercicio(request: Request):
     return templates.TemplateResponse("pages/personal/adicionar_exercicio_personal.html", {"request": request})
 
-@router.get("/adicionar_ficha_personal")  
-def get_root(request: Request):
-
+@router.get("/adicionar_ficha")  
+async def get_adicionar_ficha(request: Request):
     return templates.TemplateResponse("pages/personal/adicionar_ficha_personal.html", {"request": request})
 
-@router.get("/historico_alunos_personal")  
-def get_root(request: Request):
-
+@router.get("/historico_alunos")  
+async def get_historico_alunos(request: Request):
     return templates.TemplateResponse("pages/personal/historico_alunos_personal.html", {"request": request})
+
+@router.get("/meusalunos")  
+async def get_meusalunos(request: Request):
+    return templates.TemplateResponse("pages/personal/meus_alunos_personal.html", {"request": request})
