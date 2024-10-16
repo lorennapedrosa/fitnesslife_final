64076ec3fadbccc932c9c9223290946a1115932c
Lorenna <lorapmsilva@gmail.com>
2024-10-16 10:43:07 -0300

    adicionado tratamento de erros no pipeline da requisicao

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ from routes.personal_routes import router as personal_router
 from routes.cliente_routes import router as cliente_router
 from routes.nutricionista_routes import router as nutricionista_router
 from util.auth import checar_autenticacao, checar_autorizacao
+from util.exceptions import configurar_excecoes
 
 UsuarioRepo.criar_tabela()
 dotenv.load_dotenv()
 app = FastAPI(dependencies=[Depends(checar_autorizacao)])
 app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
 app.middleware("http")(checar_autenticacao)
+configurar_excecoes(app)
 app.include_router(main_router)
 app.include_router(usuario_router)
 app.include_router(cliente_router)
